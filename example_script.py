b46f8aeeff47111721492ad1cfd901ba6d7ed730
Dinocaridid <d.t.wooodworth@gmail.com>
2019-02-15 16:01:07 -0600

    Updated example script

--- example_script.py
+++ example_script.py
@@ -3,7 +3,7 @@
 # Import packages
 import numpy as np
 import pandas as pd
-#import maplotlib.pylot as plt
+#import matplotlib.pylot as plt
 
 # Basic loop syntax
 print('Basic loop syntax')
@@ -47,12 +47,13 @@ for x in range(10):
 print('\nIt is even possible to use else clauses with loops!')
 for x in range(0,10):
 	if x%5 == 0:
-		break
+		continue
 	print(x)
 else:
 	print('x was divisible by 5')
 
 # ***** Classes and Objects *****
+print('\nWe can define classes')
 class MyClass:
 	variable = 'MyVariable'
 	def function(self):
@@ -66,4 +67,42 @@ myobject1.variable = 'AnotherVariable'
 print(myobject1.variable)
 print(myobject2.variable)
 
+def myf(x):
+    y = x+10
+    z = x**2
+    return y,z
+myOutput = myf(2)
+print(myOutput)
 
+# ***** There are also fancy lists called "dictionaries" *****
+dict = {"name": ["Bob", "Bruce", "Bill"],
+       "grade": ["F", "F", "F-"]}
+print(dict)
+
+# ***** Now we'll look at Pandas *****
+print('\nA simple dataframe')
+studs = pd.DataFrame(dict)
+print(studs)
+
+# We can change the indices
+print('\nChanging the indices')
+studs.index = ['Bb', 'Br', 'Bl']
+print(studs)
+
+# We can read from a CSV as well
+print('\nReading from a CSV')
+ex_csv = pd.read_csv('example.csv')
+print(ex_csv)
+
+# To access parts of a dataframe:
+print("\nSelect an entire column using df['col_name']")
+print(studs['name'])
+print("\nSelect several rows using df[start#:end#]")
+print(studs[0:1])
+print("\nSelect a row by its integer index using df.iloc[#]")
+print(studs.iloc[2])
+print("\nSelect a row by name using df.loc['name']")
+print(studs.loc[['Bb']])
+
+# Excel files can also be read
+hsdt = pd.read_excel('pa_seamount_ages.xlsx',sheetname='Hawaii')
